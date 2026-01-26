372862127129cbcc5c634b829f72509bf40948f4
MREZA <rezaworku31@gmail.com>
2026-01-26 23:22:13 +0700

    created : brand api integration for / page

---
+++ src/services/brand.service.ts
@@ -0,0 +1,9 @@
+export async function getBrands() {
+    const res = await fetch('http://localhost:3000/api/brands', {
+        cache: 'no-store'
+    })
+
+    if (!res.ok) throw new Error("Failed fetch brands")
+
+    return res
+}
